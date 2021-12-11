9cc7de67b6a9d52d2f59b573c676ba6859eab581
nguyennhattan24 <nguyennhattan24>
2021-12-11 10:36:42 +0700

    fix link

--- README.md
+++
@@ -1,7 +0,0 @@
-# Call-Me
-
-- Lâm Văn Quyết
-- Tô Thế Nam
-- Nguyễn Nhật Tân
-- Phạm Anh Tuấn
-- Hoàng Minh Hiếu
--- index.js
+++ index.js
@@ -1,9 +1,37 @@
-/**
- * @format
- */
+const admin = require('firebase-admin')
+const express =require('express')
+const app = express()
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+var serviceAccount = require("./callme-302c8-firebase-adminsdk-akbzt-34678f6016.json");
+app.use(express.json())
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  
+});
 
-AppRegistry.registerComponent(appName, () => App);
+app.post('/send-noti',(req,res)=>{
+    console.log(req.body)
+    const data = {
+        dataChannel : req.body.dataChannel
+    };
+    const message ={
+        notification:{
+            title:"You have phone call",
+            body: req.body.message,    
+        },
+        token: req.body.tokens,
+        data: {
+            json: JSON.stringify(data)
+        }
+    }
+    admin.messaging().send(message).then(res =>{
+        console.log('Send call succes')
+    }).catch(err =>{
+        console.log(err)
+    })
+})
+
+app.listen(3000,()=>{
+    console.log('server running')
+})
+
